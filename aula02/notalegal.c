2bfbdc647018ffe1baf2627789a796a554b48681
unknown <2512082032@ACADEMICO.IESB.BR>
2025-03-19 10:56:44 -0300

    adicionando arquivos da praticas02

---
+++ aula02/notalegal.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+    int main() {
+        printf("-----------------------/n");
+        printf("        NOTA LEGAL       ");
+        printf("-----------------------/n");
+        printf("")
+    }
---
+++ praticas02/imprima_seu_nome.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Bruno Inacio Sousa\n");
+
+    return 0;
+}   
