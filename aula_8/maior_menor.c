fe42e6ecdaeaa095fc7e875ed1bfe47ee46d76c1
unknown <2512082032@ACADEMICO.IESB.BR>
2025-05-14 11:02:42 -0300

    adicionando arquivos da aula_8

---
+++ aula_8/maior_menor.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main{
+    numero[0] =
+    numero[1] =
+    numero[2] =
+    numero[3] =
+    numero[4] =
+    numero[5] =
+    numero[6] =
+    numero[7] = 
+    numero[8] = 2;
+    numero[9] = 0;
+
+
+
+
+
+    tem coisa aqui no meio que eu nao copiei ainda!!!
+
+
+
+
+
+
+
+
+    int maior = -99999;
+    int menor = 99999;
+
+
+    printf("entre com 10 numeros inteiros\n");
+    for(int i=0; i<10; i++) {
+        printf("numero %i: ", i+1);
+        sacnf("%i", &numero[i]);
+
+        if (maior < numeros[i]) {
+            maior = numeros[i];
+        }
+
+        if (menor > numeros[i]) {
+            menor = numeros[i];
+        }
+    }
+
+    printf("Seus numeros foram: ");
+    for(int i=0; i<10; i++) {
+        printf("%i,", numeros[i]);
+    }
+
+
+    return: 0
+
+}
